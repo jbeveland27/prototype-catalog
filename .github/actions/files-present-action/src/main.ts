ad73da7e7a84a7ae78235ee91a778e4df2a59d6a
Justin Eveland <jeveland@newrelic.com>
2020-03-06 13:00:05 -0500

    feat: using path with file check

--- .github/actions/files-present-action/src/main.ts
+++ .github/actions/files-present-action/src/main.ts
@@ -1,22 +1,26 @@
-import * as core from '@actions/core';
-import fs from 'fs';
+import * as core from "@actions/core";
+import * as path from "path";
+import fs from "fs";
 
 async function run() {
   try {
-      const files = core.getInput('files')
-      const fileList = files.split(/,\s+/)
-      var doesntExist: string[] = []
-      fileList.forEach( function( file ) {          
-          if ( ! fs.existsSync(file) ) {
-              doesntExist.push( file )
-          }
-      })
-      if (doesntExist.length > 0 ) {
-          core.setFailed( "These files do not exist: " + doesntExist.join( ", " ) )
-      }
+    const files = core.getInput("files");
+    const fileList = files.split(/,\s+/);
 
+    const inputPath: string = core.getInput("path", { required: true });
+
+    var doesntExist: string[] = [];
+    fileList.forEach(function(file) {
+      const pathedFile: string = path.join(inputPath, file);
+      if (!fs.existsSync(pathedFile)) {
+        doesntExist.push(pathedFile);
+      }
+    });
+    if (doesntExist.length > 0) {
+      core.setFailed("These files do not exist: " + doesntExist.join(", "));
+    }
   } catch (error) {
-      core.setFailed(error.message);
+    core.setFailed(error.message);
   }
 }
 
