9c9718a7d5c0f0e52dc5140d84a19e34812c8e1c
Justin Eveland <jeveland@newrelic.com>
2020-03-17 20:38:15 -0400

    feat: adding local version-check action

---
+++ .github/actions/version-check/README.md
@@ -0,0 +1,65 @@
+# Version check
+
+You can use this GitHub action to check whether your npm package version has been updated: this can be extremely helpful if you want to automate your release process.  
+The main difference between this action and many others out there is that this doesn't do a specific task (it doesn't publish to registries, create tags or releases, send notifications, ...) but instead gives you an output that you can use in other steps of your workflow as you prefer: this way you don't have to deal with stuff you don't care about ;)
+
+This action is heavily inspired by [`npm-publish-action`](https://github.com/pascalgn/npm-publish-action) by [pascal](https://github.com/pascalgn): if you only care about publishing your package to npm automatically, this is the simplest solution :thumbsup:  
+
+## Usage
+
+You have to set up a step like this in your workflow (this assumes you've already [checked out](https://github.com/actions/checkout) your repo and [set up Node](https://github.com/actions/setup-node)):
+
+```yaml
+- name: Check if version has been updated # You can edit this
+    id: check # This will be the reference for getting the outputs
+    uses: EndBug/version-check@v1 # You can choose the version/branch you prefer
+    with: # You can find more info about inputs below
+      diff-search: true
+      file-name: package.json
+      token: ${{ secrets.GITHUB_TOKEN }}
+```
+
+### Inputs
+
+- `diff-search` (optional) : whether to search in every commit's diff. This is useful if you often do change the version manually without including it in the title: you can find more info on how the action detects the version change [here](doc/logic_chain.md). If you only use `npm version` to bump versions then you can omit this.
+- `file-name` (optional) : you can use this to indicate a custom path to your `package.json`; if you keep your package file in the root directory (as every normal person would do) you can omit this.
+- `token` (optional) : you can put your bearer GitHub token here. This is needed only when running the action on private repostiories, if you're running it on a public repo you can omit this. If you need to set this, you can use the built-in `GITHUB_TOKEN` secret that GitHub generates for your repo's actions: you cna find more info about it [here](https://help.github.com/en/github/automating-your-workflow-with-github-actions/virtual-environments-for-github-actions#github_token-secret).
+
+### Outputs
+
+This action sets two outputs:
+
+- `changed` : either "true" or "false", indicates whether the version has changed.
+- `type` : if the version has changed, it tries to find the type of bump (e.g. "patch", "minor", ...)
+- `version` : if the version has changed, it shows the version number (e.g. "1.0.2")
+
+To access these outputs, you need to access the context of the step you previously set up: you can find more info about steps contexts [here](https://help.github.com/en/articles/contexts-and-expression-syntax-for-github-actions#steps-context).  
+If you set your step id to `check` you'll find the outputs at `steps.check.outputs.changed`, `steps.check.outputs.type` and `steps.check.outputs.version`: you can use these outputs as conditions for other steps.  
+Here's an example:
+
+```yaml
+- name: Check if version has been updated
+  id: check
+  uses: EndBug/version-check@v1
+
+- name: Log when changed
+  if: steps.check.outputs.changed == 'true'
+  run: 'echo "Version change found! New version: ${{ steps.check.outputs.version }} (${{ steps.check.outputs.type }})"'
+
+- name: Log when unchanged
+  if: steps.check.outputs.changed != 'true'
+  run: 'echo "No version change :/"'
+```
+
+### Publishing automatically to both NPM & GitHub Package Registry
+
+If you want to see how to publish automatically your package to both NPM & GPR, please see [this](doc/auto-publish-example.yml) example workflow ;)
+You can also find a more in-depth guide in this [here](doc/auto-publish-walkthrough.md).
+
+## Contributing
+
+If you want to contribute to the action, even by just raising a problem or proposing an idea, you can click [here](CONTRIBUTING.md) to find out how to do it ;)
+
+## License
+
+This action is distributed under the MIT license, check the [license](LICENSE) for more info.
---
+++ .github/actions/version-check/src/main.ts
@@ -0,0 +1,274 @@
+import * as core from '@actions/core'
+import axios from 'axios'
+import { readFile } from 'fs'
+import { join } from 'path'
+import semverDiff from 'semver-diff'
+import semverRegex from 'semver-regex'
+
+const packageFileName = core.getInput('file-name') || 'package.json',
+  dir = process.env.GITHUB_WORKSPACE || '/github/workspace',
+  eventFile = process.env.GITHUB_EVENT_PATH || '/github/workflow/event.json',
+  token = core.getInput('token')
+
+type ArgValue<T> =
+  T extends 'changed' ? boolean :
+  T extends 'type' ? string | undefined :
+  T extends 'version' ? string | undefined :
+  never
+
+async function main() {
+  const eventObj = await readJson(eventFile)
+  return await processDirectory(dir, eventObj.commits)
+}
+
+interface Commit {
+  id: string
+  message: string
+  author: {
+    name: string
+    email: string
+  }
+  url: string
+  distinct: boolean
+}
+
+interface PackageObj {
+  version: string
+}
+function isPackageObj(value): value is PackageObj {
+  return !!value && !!value.version
+}
+
+async function getCommit(sha: string): Promise<CommitResponse> {
+  const url = `https://api.github.com/repos/${process.env.GITHUB_REPOSITORY}/commits/${sha}`
+  const headers = token ? {
+    Authorization: `Bearer ${token}`
+  } : undefined
+  return (await axios.get(url, { headers })).data
+}
+interface CommitResponse {
+  url: string
+  sha: string
+  node_id: string
+  html_url: string
+  comments_url: string
+  commit: {
+    url: string
+    author: {
+      name: string
+      email: string
+      date: string
+    }
+    committer: {
+      name: string
+      email: string
+      date: string
+    }
+    message: string
+    tree: {
+      url: string
+      sha: string
+    }
+    comment_count: number
+    verification: {
+      verified: boolean
+      reason: string
+      signature: object | null
+      payload: object | null
+    }
+  }
+  author: {
+    login: string
+    id: number
+    node_id: string
+    avatar_url: string
+    gravatar_id: string
+    url: string
+    html_url: string
+    followers_url: string
+    following_url: string
+    gists_url: string
+    starred_url: string
+    subscriptions_url: string
+    organizations_url: string
+    repos_url: string
+    events_url: string
+    receive_events_url: string
+    type: string
+    site_admin: boolean
+  }
+  committer: {
+    login: string
+    id: number
+    node_id: string
+    avatar_url: string
+    gravatar_id: string
+    url: string
+    html_url: string
+    followers_url: string
+    following_url: string
+    gists_url: string
+    starred_url: string
+    subscriptions_url: string
+    organizations_url: string
+    repos_url: string
+    events_url: string
+    receive_events_url: string
+    type: string
+    site_admin: boolean
+  }
+  parents: {
+    url: string
+    sha: string
+  }[]
+  stats: {
+    additions: number
+    deletions: number
+    total: number
+  }
+  files: {
+    filename: string
+    additions: number
+    deletions: number
+    changes: number
+    status: string
+    raw_url: string
+    blob_url: string
+    patch: string
+  }[]
+}
+
+
+async function checkCommits(commits: Commit[], version: string) {
+  try {
+    for (const commit of commits) {
+      const match = commit.message.match(semverRegex()) || []
+      if (match.includes(version)) {
+        if (await checkDiff(commit.id, version)) {
+          console.log(`Found match for version ${version}: ${commit.id.substring(0, 7)} ${commit.message}`)
+          return true
+        }
+      }
+    }
+
+    if (core.getInput('diff-search')) {
+      console.log('No standard npm version commit found, switching to diff search (this could take more time...)')
+
+      for (const commit of commits) {
+        if (await checkDiff(commit.id, version)) {
+          console.log(`Found match for version ${version}: ${commit.id.substring(0, 7)} ${commit.message}`)
+          return true
+        }
+      }
+    }
+
+    console.log('No matching commit found.')
+    return false
+  } catch (e) {
+    core.setFailed(e)
+  }
+}
+
+async function checkDiff(sha: string, version: string) {
+  try {
+    const commit = await getCommit(sha)
+    const pkg = commit.files.find(f => f.filename == packageFileName)
+    if (!pkg) return false
+
+    const versionLines: {
+      added?: string
+      deleted?: string
+    } = {}
+
+    const rawLines = pkg.patch.split('\n')
+      .filter(line => line.includes('"version":') && ['+', '-'].includes(line[0]))
+    if (rawLines.length > 2) return false
+
+    for (const line of rawLines)
+      versionLines[line.startsWith('+') ? 'added' : 'deleted'] = line
+    if (!versionLines.added) return false
+
+    const versions = {
+      added: matchVersion(versionLines.added),
+      deleted: !!versionLines.deleted && matchVersion(versionLines.deleted)
+    }
+    if (versions.added != version) return false
+
+    setOutput('changed', true)
+    setOutput('version', version)
+    if (versions.deleted)
+      setOutput('type', semverDiff(versions.deleted, versions.added))
+    return true
+  } catch (e) {
+    console.error(`An error occurred in checkDiff:\n${e}`)
+    throw new ExitError(1)
+  }
+}
+
+function matchVersion(str: string) {
+  return ((str.split('"') || [])
+    .map(s => s.match(semverRegex()))
+    .find(e => !!e) || [])[0]
+}
+
+async function processDirectory(dir: string, commits: Commit[]) {
+  try {
+    const packageFile = join(dir, packageFileName),
+      packageObj = await readJson(packageFile).catch(() => {
+        Promise.reject(
+          new NeutralExitError(`Package file not found: ${packageFile}`)
+        )
+      })
+
+    if (!isPackageObj(packageObj))
+      throw new Error('Can\'t find version field')
+
+    if (commits.length >= 20)
+      console.warn('This workflow run topped the commit limit set by GitHub webhooks: that means that commits could not appear and that the run could not find the version change.')
+
+    await checkCommits(commits, packageObj.version)
+  } catch (e) {
+    core.setFailed(e)
+  }
+}
+
+async function readJson(file: string) {
+  const data: string = await new Promise((resolve, reject) =>
+    readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(data)
+      }
+    })
+  )
+  return JSON.parse(data)
+}
+
+function setOutput<T extends 'changed' | 'type' | 'version'>(name: T, value: ArgValue<T>) {
+  return core.setOutput(name, `${value}`)
+}
+
+// #region Error classes
+class ExitError extends Error {
+  code?: number
+
+  constructor(code: number | null) {
+    super(`Command failed with code ${code}`)
+    if (typeof code == 'number') this.code = code
+  }
+}
+
+class NeutralExitError extends Error { }
+// #endregion
+
+if (require.main == module) {
+  console.log('Searching for version update...')
+  main().catch(e => {
+    if (e instanceof NeutralExitError) process.exitCode = 78
+    else {
+      process.exitCode = 1
+      console.error(e.message || e)
+    }
+  })
+}
