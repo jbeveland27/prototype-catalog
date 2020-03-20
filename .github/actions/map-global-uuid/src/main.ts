7eda2153a3ad51c8c28ffdc070fdb413a3e8607d
Justin Eveland <jeveland@newrelic.com>
2020-03-20 10:43:34 -0400

    fix: another one

--- .github/actions/map-global-uuid/src/main.ts
+++ .github/actions/map-global-uuid/src/main.ts
@@ -41,11 +41,53 @@ async function run() {
       //   nr1 nerdpack:uuid -gf
       console.debug(`Generating new uuid for submodule: ${submoduleName}`)
       console.debug(`Path: ${submodulePath}`)
-      const nr1 = await io.which('nr1', true)
-      const options: {[key: string]: string} = {}
-      options.cwd = submodulePath
-      await exec.exec(`"${nr1}"`, ['nerdpack:info'], {cwd: submodulePath})
+      // const nr1 = await io.which('nr1', true)
+      // const options: {[key: string]: string} = {}
+      // options.cwd = submodulePath
+      // await exec.exec(`"${nr1}"`, ['nerdpack:info'], {cwd: submodulePath})
 
+      const {exec} = require('child_process')
+      exec('cd', (err: any, stdout: any, stderr: any) => {
+        if (err) {
+          //some err occurred
+          console.error(`exec error: ${err}`)
+          core.setFailed(`map-global-uuid failed: ${err.message}`)
+        } else {
+          exec('nr1 nerdpack:info', (err: any, stdout: any, stderr: any) => {
+            if (err) {
+              //some err occurred
+              console.error(`exec error: ${err}`)
+              core.setFailed(`map-global-uuid failed: ${err.message}`)
+            } else {
+              // the *entire* stdout and stderr (buffered)
+              console.log(`stdout: ${stdout}`)
+              console.log(`stderr: ${stderr}`)
+
+              // Get generated uuid from nr1.json
+              const nr1JsonPath: string = path.join(submodulePath, 'nr1.json')
+              const nr1Json = require(nr1JsonPath)
+              console.debug('nr1Json after generation: ', nr1Json)
+              console.debug('globalsJson: ', globalsJson)
+
+              // Copy uuid out of nr1.json into globals.json
+              const uuid = nr1Json.hasOwnProperty('id') ? nr1Json.id : ''
+              globalsJson.submoduleName = uuid
+              fs.writeFile(
+                globalsJsonPath,
+                JSON.stringify(globalsJson, null, 2),
+                function writeJSON(err) {
+                  if (err) return console.log(err)
+                  console.debug(
+                    'globals after writing uuid: ',
+                    JSON.stringify(globalsJson, null, 2)
+                  )
+                  console.debug(`Writing to: ${globalsJsonPath}`)
+                }
+              )
+            }
+          })
+        }
+      })
       // const {exec, spawn} = require('child_process')
 
       // const pwd = spawn('pwd', [], {cwd: submodulePath})
