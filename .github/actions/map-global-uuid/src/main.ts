e2508f4609690d20930fdcd79dac63d3f74c63d0
Justin Eveland <jeveland@newrelic.com>
2020-03-20 00:41:39 -0400

    fix: adding cwd option

--- .github/actions/map-global-uuid/src/main.ts
+++ .github/actions/map-global-uuid/src/main.ts
@@ -44,33 +44,39 @@ async function run() {
       // await exec.exec(`"${nr1}" nerdpack:uuid -gf`, [], {cwd: submodulePath})
 
       const {exec} = require('child_process')
-      exec(`cd ${submodulePath} ; nr1 nerdpack:uuid -gf`, (err: any, stdout: any, stderr: any) => {
-        if (err) {
-          //some err occurred
-          console.error(err)
-        } else {
-          // the *entire* stdout and stderr (buffered)
-          console.log(`stdout: ${stdout}`)
-          console.log(`stderr: ${stderr}`)
+      exec(
+        'nr1 nerdpack:uuid -gf',
+        {
+          cwd: submodulePath
+        },
+        (err: any, stdout: any, stderr: any) => {
+          if (err) {
+            //some err occurred
+            console.error(err)
+          } else {
+            // the *entire* stdout and stderr (buffered)
+            console.log(`stdout: ${stdout}`)
+            console.log(`stderr: ${stderr}`)
 
-          // Get generated uuid from nr1.json
-          const nr1JsonPath: string = path.join(submodulePath, 'nr1.json')
-          const nr1Json = require(nr1JsonPath)
-          console.debug('nr1Json after generation: ', nr1Json)
-          console.debug('globalsJson: ', globalsJson)
+            // Get generated uuid from nr1.json
+            const nr1JsonPath: string = path.join(submodulePath, 'nr1.json')
+            const nr1Json = require(nr1JsonPath)
+            console.debug('nr1Json after generation: ', nr1Json)
+            console.debug('globalsJson: ', globalsJson)
 
-          // Copy uuid out of nr1.json into globals.json
-          const uuid = nr1Json.hasOwnProperty('id') ? nr1Json.id : ''
-          globalsJson.submoduleName = uuid
-          fs.writeFile(globalsJsonPath, JSON.stringify(globalsJson, null, 2), function writeJSON(
-            err
-          ) {
-            if (err) return console.log(err)
-            console.debug('globals after writing uuid: ', JSON.stringify(globalsJson, null, 2))
-            console.debug(`Writing to: ${globalsJsonPath}`)
-          })
+            // Copy uuid out of nr1.json into globals.json
+            const uuid = nr1Json.hasOwnProperty('id') ? nr1Json.id : ''
+            globalsJson.submoduleName = uuid
+            fs.writeFile(globalsJsonPath, JSON.stringify(globalsJson, null, 2), function writeJSON(
+              err
+            ) {
+              if (err) return console.log(err)
+              console.debug('globals after writing uuid: ', JSON.stringify(globalsJson, null, 2))
+              console.debug(`Writing to: ${globalsJsonPath}`)
+            })
+          }
         }
-      })
+      )
       // TODO: Commit globals.json (in PR?)
     }
   } catch (error) {
